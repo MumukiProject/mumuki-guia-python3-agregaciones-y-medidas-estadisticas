a254d4132ddc535d56b753807778fa4fa3981f97
franco@mumuki.org <franco@mumuki.org>
2022-09-28 17:14:11 +0000

    Mumuki Export on 2022-09-28 17:14:11 +0000

--- 00011_Un promedio selectivo/test.py
+++ 00011_Un promedio selectivo/test.py
@@ -1,4 +1,13 @@
 class Test(unittest.TestCase):
 
-  def test_description_example(self):
-    self.assertTrue(True)
+  def test_promedio_de_los_primeros_10_10_5_9_100_250_con_n_2(self):
+    self.assertEquals(promedio_de_los_primeros(pd.Series([10, 10, 5, 9, 100, 250]), 2), 10)
+
+  def test_promedio_de_los_primeros_10_10_5_9_100_250_con_n_4(self):
+    self.assertEquals(promedio_de_los_primeros(pd.Series([10, 10, 5, 9, 100, 250]), 4), 8.5)
+
+  def test_promedio_de_los_primeros_10_10_5_9_100_250_con_n_1(self):
+    self.assertEquals(promedio_de_los_primeros(pd.Series([10, 10, 5, 9, 100, 250]), 1), 10)
+
+  def test_promedio_de_los_primeros_1000_42_16_89_75_45_con_n_4(self):
+    self.assertEquals(promedio_de_los_primeros(pd.Series([10, 10, 5, 9, 100, 250]), 4), 286.75)
