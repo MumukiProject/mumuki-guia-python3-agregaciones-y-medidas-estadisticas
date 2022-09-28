e22fb51e6b2147b0be2825202b09226a91d90f79
franco@mumuki.org <franco@mumuki.org>
2022-09-28 17:19:31 +0000

    Mumuki Export on 2022-09-28 17:19:31 +0000

--- 00012_De punta a punta/test.py
+++ 00012_De punta a punta/test.py
@@ -1,4 +1,10 @@
 class Test(unittest.TestCase):
 
-  def test_description_example(self):
-    self.assertTrue(True)
+  def test_diferencia_de_extremos_10_5_100(self):
+    self.assertEquals(diferencia_de_extremos(pd.Series([10, 5, 100])), 94)
+
+  def test_diferencia_de_extremos_10_10_5_9_100_250(self):
+    self.assertEquals(diferencia_de_extremos(pd.Series([10, 10, 5, 9, 100, 250])), 245)
+
+  def test_diferencia_de_extremos_1000_42_16_89_75_45(self):
+    self.assertEquals(diferencia_de_extremos(pd.Series([1000, 42, 16, 89, 75, 45])), 984)
